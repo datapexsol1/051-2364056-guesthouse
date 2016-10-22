650aad275736c552d6d078346ab5d5df8089f7ba
maria <maryahchaudhry@gmail.com>
2016-10-22 13:26:33 +0500

    billclass

--- App_Code/billclass.cs
+++ App_Code/billclass.cs
@@ -21,12 +21,12 @@ public class billclass
     {
         bool return_ = false;
         ctownDataContext db = new ctownDataContext();
-        int count = (from x in db.bills 
-                     where x.Id == b.Id && x.Date==b.Date && x.BillType==b.BillType     //for checking already existance
+        int count = (from x in db.bills
+                     where x.Id == b.Id && x.Date == b.Date && x.BillType == b.BillType   //for checking already existance
                      select x).Count();
         if (count == 0)
         {
-            
+
             try
             {
                 db.bills.InsertOnSubmit(b);
@@ -37,7 +37,7 @@ public class billclass
             {
                 return_ = false;
             }
-          
+
         }
         return return_;
     }
@@ -48,33 +48,97 @@ public class billclass
     {
         ctownDataContext db = new ctownDataContext();
         IQueryable<bill> bil = from b in db.bills
-                               where b.BranchId==bid
-                             select b;
+                               where b.BranchId == bid
+                               select b;
         return bil;
     }
 
 
 
 
-    public static void updateBills(bill b, int billid)
+    public static bool updateBills(bill b, int branchID)
     {
 
         ctownDataContext db = new ctownDataContext();
         var ra = (from x in db.bills
-                  where x.Id == billid
+                  where x.BranchId == branchID && x.BillType == b.BillType && x.Date == b.Date
                   select x).First();
-       
+
         ra.BillAmount = b.BillAmount;
-        ra.BillType = b.BillType;
-        ra.Date = b.Date;
-   //     int check = (from y in db.bills
-    //                 where y.Id == ra.Id
-     //                select y).Count();
-     //   if (check == 0)
-     //   {
+        // ra.BillType = b.BillType;
+        //ra.Date = b.Date;
+        int check = (from y in db.bills
+                     where y.Id == ra.Id
+                     select y).Count();
+        if (check == 1)
+        {
             db.SubmitChanges();
-        //}
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
     }
 
+    public static IQueryable<bill> getAllbills(int pid)
+    {
+        ctownDataContext db = new ctownDataContext();
+        IQueryable<bill> bil = from b in db.bills
+                               select b;
+        return bil;
+    }
+    public static IQueryable<bill> getAllBills(int bid)
+    {
+        ctownDataContext db = db = new ctownDataContext();
+        IQueryable<bill> ra = from x in db.bills
+                                  //  join r in db.rooms on new { X1 = x.room_id, X2 = bid } equals new { X1 = r.Id, X2 = r.branch_id }   //and in linq 
+                              where x.BranchId == bid
+                              select x;
+        return ra;
+    }
+    public static int retrieveBillItem(string val, DateTime date, int branchId)
+    {
 
+        ctownDataContext db = db = new ctownDataContext();
+        int billid = (from b in db.GetTable<bill>()
+                      where b.BillType == val && b.Date == date && b.BranchId == branchId
+                      select b.Id).First();
+
+        return billid;
+
+    }
+    public static bill getBills(int id, DateTime date, string type, int branchID)
+    {
+
+        ctownDataContext db = db = new ctownDataContext();
+        bill b = (from x in db.bills
+                      //  join r in db.rooms on new { X1 = x.room_id, X2 = bid } equals new { X1 = r.Id, X2 = r.branch_id }   //and in linq 
+                  where x.Id == id && x.Date == date && x.BillType == type && x.BranchId == branchID
+                  select x).First();
+        return b;
+    }
+    public static List<bill> getBillItem(string val, int branchId)
+    {
+
+        ctownDataContext db = db = new ctownDataContext();
+
+        List<bill> billid = (from b in db.GetTable<bill>()
+                             where b.BranchId == branchId
+                             select b).ToList();
+        List<bill> selectedbills = new List<bill>();
+        foreach (bill x in billid)
+        {
+            TimeSpan subtractdate = (DateTime.Now.Subtract(x.Date));
+            int days = subtractdate.Days;
+            if (days == 0)
+            {
+                //bill b = (bill)x;
+                selectedbills.Add(x);
+            }
+        }
+        return selectedbills;
+
+    }
 }
