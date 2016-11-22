1b7b98f1c38eba929d924bf72e2f2b10f8cedb98
waqaskhan <waqaskhan132@gmail.com>
2016-11-22 15:31:06 +0500

    ccccc

--- App_Code/events.cs
+++ App_Code/events.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Globalization;
 
 /// <summary>
 /// Summary description for events
@@ -57,7 +58,7 @@ public class events
         int check = (from y in db.event_calenders
                      where y.event_name == updatedEvent.event_name && y.event_description==updatedEvent.event_description && y.event_color == updatedEvent.event_color
                      select y).Count();
-        if (check == 0)
+        if (check == 1)
         {
             db.SubmitChanges();
             checkbool = true;
@@ -82,7 +83,13 @@ public class events
                                          join even in db.event_calenders on x.Id equals even.employee_id
                                          where x.branch_id == bid
                                          select even;
-                               
+             //change the format of date 
+
+        foreach(event_calender c in ec)
+        {
+            c.event_start_date = DateTime.Parse(c.event_start_date.ToString("dd/M/yyyy", CultureInfo.InvariantCulture));
+                c.event_end_date= DateTime.Parse(c.event_end_date.ToString("dd/M/yyyy", CultureInfo.InvariantCulture));
+        }
                                
         return ec;
     }
